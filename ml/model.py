3b2974ca2b7fc1bee05aa77cf352ad1fd90a1a3a
radioactivecats7 <kevinwallace4774@gmail.com>
2026-05-27 19:22:51 -0400

    Completed ml/model.py

--- ml/model.py
+++ ml/model.py
@@ -1,4 +1,6 @@
+import joblib
 import pickle
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
@@ -19,8 +21,9 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    model = RandomForestClassifier(n_estimators=100, random_stats=42)
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -59,8 +62,8 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+    preds = model.predict(X)
+    return preds
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -72,13 +75,11 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    joblib.dump(model, path)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    return joblib.load(path)
 
 
 def performance_on_categorical_slice(
@@ -119,6 +120,12 @@ def performance_on_categorical_slice(
     """
     # TODO: implement the function
     X_slice, y_slice, _, _ = process_data(
+        data[data[column_name] == slice_value],
+        categorical_features=categorical_features, 
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
