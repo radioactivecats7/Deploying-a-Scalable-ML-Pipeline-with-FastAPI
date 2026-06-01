e1c1c66d5e72230e4fef7355c861378b16f852fe
radioactivecats7 <kevinwallace4774@gmail.com>
2026-06-01 16:29:44 -0400

    fixed missing item in model.py

--- ml/model.py
+++ ml/model.py
@@ -130,6 +130,6 @@ def performance_on_categorical_slice(
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
