b9edd630ea15102dc93585165e26be94505406ed
radioactivecats7 <kevinwallace4774@gmail.com>
2026-06-03 20:42:26 -0400

    Fixed local_api.py

--- local_api.py
+++ local_api.py
@@ -30,7 +30,7 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = requests.post("http://127.0.0.1:8000/data/", json=data) # Your code here
+r = requests.post("http://127.0.0.1:8000/data/", json=data) 
 
 # TODO: print the status code
 print(f"Status code: {r.status_code}")
